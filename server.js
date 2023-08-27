75283b30e4ff0fb94dc10dc18aee14613f16dbc0
Siddhant Singh <siddhantsingh9023134084@gmail.com>
2023-08-27 19:29:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import { app } from "./app.js";
 
 app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+    console.log(`listening on port ${process.env.PORT} in ${process.env.NODE_ENV} mode ,CORS valid for ${process.env.FRONTEND_URI}`);
   });
